1e212e5a79f03dee49a264beb6162c71a5a2e006
Todd Ivey <toddivey@gmail.com>
2019-04-02 10:35:16 -0500

    bubblesort

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+var array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
+// swap function helper
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+// be careful: this is a very basic implementation which is nice to understand the deep principle of bubble sort (going through all comparisons) but it can be greatly improved for performances
+function bubbleSort(array) {
+  for(var i = 0; i < array.length; i++) {
+    for(var j = 1; j < array.length; j++) {
+      if(array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
+}
